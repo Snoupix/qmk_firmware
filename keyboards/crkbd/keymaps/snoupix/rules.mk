1ebd56bb40cc95a5f7b2a4269ac2e8185ab48ef4
Snoupix <samufpsdu59@hotmail.fr>
2024-06-27 14:46:44 +0200

    feat(keymap): Add Snoupix keymap with Bongocat on the left (master) and Luna on the right

---
+++ keyboards/crkbd/keymaps/snoupix/rules.mk
@@ -0,0 +1,17 @@
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+
+TAP_DANCE_ENABLE = no
+EXTRAKEY_ENABLE = yes
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+
+WPM_ENABLE = yes
+OLED_ENABLE = yes
+SRC += oled_icons.c
+# SRC += oled_luna.c oled_bongocat.c oled_icons.c
+# SRC += oled_bongocat.c oled_icons.c
+# SRC += oled_luna.c oled_icons.c
+
+#EXTRAFLAGS += -flto
+#RGB_MATRIX_SPLIT_RIGHT=yes
