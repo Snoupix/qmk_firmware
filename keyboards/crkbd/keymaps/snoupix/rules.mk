bb9d6a80a26b931d58558f2438e494794a186d04
Snoupix <samufpsdu59@hotmail.fr>
2024-06-30 13:37:01 +0200

    feat(oled): Both oled screens are displaying bongocat && luna + Layout fixes and RGB config

--- keyboards/crkbd/keymaps/snoupix/rules.mk
+++ keyboards/crkbd/keymaps/snoupix/rules.mk
@@ -1,17 +1,22 @@
+# https://github.com/qmk/qmk_firmware/blob/master/docs/config_options.md
+
+# RGB stuff
+# https://github.com/qmk/qmk_firmware/tree/master/keyboards/crkbd#rgb-matrix
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
+# RGB_MATRIX_SPLIT_RIGHT=yes
 
+# Extra features
+VIA_ENABLE = yes
 TAP_DANCE_ENABLE = no
 EXTRAKEY_ENABLE = yes
-VIA_ENABLE = yes
-LTO_ENABLE = yes
+MOUSEKEY_ENABLE = no
 
+# Oled screens
 WPM_ENABLE = yes
 OLED_ENABLE = yes
 SRC += oled_icons.c
-# SRC += oled_luna.c oled_bongocat.c oled_icons.c
-# SRC += oled_bongocat.c oled_icons.c
-# SRC += oled_luna.c oled_icons.c
 
-#EXTRAFLAGS += -flto
-#RGB_MATRIX_SPLIT_RIGHT=yes
+# Needed
+LTO_ENABLE = yes
+SPLIT_KEYBOARD = yes
